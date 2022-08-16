ff37ddec6e20a4a327d48b0e3a26983e90081072
Le Ngoc Danh <lengocdanh123456@gmail.com>
2022-08-16 16:38:43 +0700

    source tree not working

---
+++ lib/screens/create_new_food.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CreateNewFood extends StatefulWidget {
+  const CreateNewFood({Key? key}) : super(key: key);
+
+  @override
+  State<CreateNewFood> createState() => _CreateNewFoodState();
+}
+
+class _CreateNewFoodState extends State<CreateNewFood> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
